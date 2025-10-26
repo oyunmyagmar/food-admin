a59ec47a578d9588039ea32343ea92fc675fcbc3
oyunmyagmar <25LP4018@nest.edu.mn>
2025-10-27 00:22:13 +0900

    d

--- src/app/_components/AdminLayout.tsx
+++ src/app/_components/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { SideBar, Header } from "@/app/_components";
 
 export const AdminLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-screen h-screen flex justify-center bg-secondary m-0">
+    <div className="w-screen h-screen flex justify-center bg-secondary m-auto">
       <SideBar />
       <div>
         <Header />
